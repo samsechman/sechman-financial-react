f6b78c47caeb7ce6afea3285f1d583eeb5ac61ca
Samuel <samsechman@gmail.com>
2025-03-29 17:40:03 -0400

    Configure for GitHub Pages with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import { AuthProvider } from './context/AuthContext';
 import Layout from './components/layout/Layout';
